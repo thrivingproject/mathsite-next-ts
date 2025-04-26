2e6f725fd3027854a2c271744928c021e0bd1695
Christian Isaman <christianisaman@outlook.com>
2025-04-26 18:08:14 +0800

    add rational number topic

--- app/topic/rational_numbers/page.jsx
+++ app/topic/rational_numbers/page.jsx
@@ -8,10 +8,16 @@ export default function Page() {
         <header>
           <h2>Rational numbers</h2>
         </header>
-        <p></p>
+        <p>
+          A number of the form {"`a/b`"} where {"`a`"} and {"`b`"} are{" "}
+          <Link href={"/topic/integers"}>integers</Link> and {"`b != 0`"}. Numbers that have a
+          repeating decimal pattern are also rational.
+        </p>
         <section>
           <h3>Examples</h3>
-          <p></p>
+          <p>{"`1/2 = 0.5`"}</p>
+          <p>{"`1/3 = 0.bar 333`"}</p>
+          <p>{'`1/7 = 0.bar 142857 = 0.142857142857...`'}</p>
         </section>
       </article>
     </MathJax>
