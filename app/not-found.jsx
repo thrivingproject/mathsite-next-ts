1541f369afbfb7a2941aa114b3a7809de7cd6bfc
Christian Isaman <christianisaman@outlook.com>
2025-04-26 19:15:58 +0800

    feat: enhance NotFound component with back navigation link

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,23 @@
-import Link from 'next/link'
- 
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <div>
       <h2>Yikes, misplaced topic!</h2>
       <p>This page does not exist.</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/">Home</Link>
+      <span>&nbsp;&nbsp;</span>
+      <Link href={"/"} onNavigate={handleBack}>
+        Back
+      </Link>
     </div>
-  )
+  );
 }
