594a30c73b94c165e0b8c510e5258d1b322a5b24
Christian Isaman <christianisaman@outlook.com>
2025-05-04 16:15:51 +0800

    fix: make layout rerender so mathjax reevaluates asciimath when routing

--- app/(subjects)/layout.tsx
+++ app/(subjects)/layout.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import Nav from "@/components/Nav";
 import { MathJax } from "better-react-mathjax";
+import { usePathname } from "next/navigation";
 
+/**
+ * This is a client component so `usePathname()` can be added as a key,
+ * which will cause the component to re-render when the pathname changes.
+ * This is needed so that MathJax can re-render the math when routing to new page.
+ */
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const path = usePathname();
   return (
-    <>
+    <MathJax key={path}>
       <Nav />
-      <main>
-        <MathJax>{children}</MathJax>
-      </main>
-    </>
+      <main>{children}</main>
+    </MathJax>
   );
 }
